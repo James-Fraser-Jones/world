37e46925e47c6133536bda6e6d5f16dad5adcfbb
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-04-19 22:13:18 +0100

    first triangle, baby

--- world/example.cpp
+++ world/example.cpp
@@ -14,7 +14,35 @@ https://www.youtube.com/watch?v=QM4WW8hcsPU&list=PLvv0ScY6vfd-p1gSnbQhY7vMe2rng0
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//vertex shader code
+const char* vertexShaderSource = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(0.035f, 0.603f, 0.117f, 0.0f);\n"
+    "}\n";
+
+//vertices for a triangle in 3D space (in normalized device coordinates i.e. +/- 1)
+const float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
 int main(int argc, char* argv[]) {
+
+    /******************************************************
+    * setup SDL and OpenGL
+    ******************************************************/
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0){
         std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\n";
@@ -22,7 +50,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    //specify OpenGL version
+    //specify OpenGL version and profile
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -47,7 +75,81 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    /******************************************************
+    * setup shaders and shader program
+    ******************************************************/
+
+    //create vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); //store unique shader ID
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //set shader source code
+    glCompileShader(vertexShader); //compile shader
+    int  success; //shader compilation error handling
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //create fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //create shader program (linking vertex and fragment shaders)
+    unsigned int shaderProgram = glCreateProgram(); //store unique program ID
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success); //shader program linking error handling
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    //delete shaders now they have been linked into shader object
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    /******************************************************
+    * configure vertex data
+    ******************************************************/
+
+    //create vertex array object (VAO) to store all info for a set of vertices
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); //get 1 vertex array object ID
+    glBindVertexArray(VAO); //bind this vertex array so that VBO configuration gets added to this object
+
+    //send vertex data to the gpu
+    unsigned int VBO; //array for storing unique buffer object IDs
+    glGenBuffers(1, &VBO); //get 1 buffer object ID
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind buffer type to vertex buffer object (VBO)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //copy vertex data into the buffer (GL_STATIC_DRAW means used a lot but unchanging)
+
+    //specify how to attach vertex attributes to vertex shader (using data currently bound to GL_ARRAY_BUFFER)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    /*
+    arguments:
+    at location "0" (i.e. aPos attribute in vertex shader),
+    aPos is a vec3 so contains "3" values,
+    values are "GL_FLOAT"s,
+    normalize values ? "GL_FALSE",
+    size of aPos? "3 * sizeof(float)"
+    offset to read from? "0" (but has to be cast to void* for some reason
+    */
+
+    //enable vertex attribute aPos at location "0"
+    glEnableVertexAttribArray(0);
+
+    /******************************************************
+    * enter rendering loop
+    ******************************************************/
 
     bool running = true;
     while (running) {
@@ -76,6 +178,10 @@ int main(int argc, char* argv[]) {
         //rendering commands (here we're just clearing the screen to gold)
         glClearColor(0.99f, 0.76f, 0.0f, 1.0f); //set screen color
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram); //use our set of shaders
+        glBindVertexArray(VAO); //use our set of configured vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3); //draw triangles (start at vertex 0, draw 3 vertices)
 
         //update window using swapchain
         SDL_GL_SwapWindow(window);
